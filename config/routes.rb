adabec31fd81b2433594d656265528deba7be24e
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-07-14 09:58:24 +0100

    chore(route): modified the route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
+  root 'welcome#index'
   get 'blogapp/about', to: 'blogapp#about'
 
   resources :articles
