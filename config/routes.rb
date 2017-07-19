f09847e60b4842fd084890e81cce7843ff35bf2a
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-07-19 15:34:59 +0100

    feat(userPage): create actions for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'blogapp/about', to: 'blogapp#about'
 
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+
+  resources :users
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :articles
 end
